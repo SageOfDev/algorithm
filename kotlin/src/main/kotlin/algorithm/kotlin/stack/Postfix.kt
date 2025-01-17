e9ff8291f875aa0f61f27135ce784a9c3ecd8730
승종 배 <succeeding@nexon.co.kr>
2025-01-17 15:52:07 +0900

    refactor: postfix

---
+++ kotlin/src/main/kotlin/algorithm/kotlin/stack/Postfix.kt
@@ -0,0 +1,96 @@
+package algorithm.kotlin
+
+import java.util.*
+
+// 백준 1918
+fun boj1918() {
+    System.`in`.bufferedReader().use { br ->
+        val infixNotation = br.readLine()
+        val answer = infixToPostfix(infixNotation)
+        println(answer)
+    }
+}
+
+fun infixToPostfix(infixNotation: String): String {
+    val postfixNotation = StringBuilder()
+    val operatorStk = Stack<Char>()
+    val priority = mapOf('+' to 1, '-' to 1, '*' to 2, '/' to 2) // '(' 제외
+
+    infixNotation.forEach { ch ->
+        when {
+            ch.isLetterOrDigit() -> postfixNotation.append(ch) // 피연산자 처리
+            ch == '(' -> operatorStk.push(ch) // 여는 괄호는 스택에 무조건 push
+            ch == ')' -> {
+                // 닫는 괄호는 여는 괄호가 나올 때까지 연산자를 pop
+                while (operatorStk.isNotEmpty() && operatorStk.peek() != '(') {
+                    postfixNotation.append(operatorStk.pop())
+                }
+                if (operatorStk.isEmpty() || operatorStk.pop() != '(') {
+                    throw IllegalArgumentException("Mismatched parentheses in input expression")
+                }
+            }
+            ch in priority -> {
+                // 현재 연산자와 스택의 top 연산자 우선순위 비교
+                while (operatorStk.isNotEmpty() && operatorStk.peek() != '(' &&
+                    priority[operatorStk.peek()]!! >= priority[ch]!!
+                ) {
+                    postfixNotation.append(operatorStk.pop())
+                }
+                operatorStk.push(ch)
+            }
+            else -> throw IllegalArgumentException("Invalid character in input expression: $ch")
+        }
+    }
+
+    // 남은 연산자를 postfixNotation에 추가
+    while (operatorStk.isNotEmpty()) {
+        val top = operatorStk.pop()
+        if (top == '(') throw IllegalArgumentException("Mismatched parentheses in input expression")
+        postfixNotation.append(top)
+    }
+
+    return postfixNotation.toString()
+}
+
+fun operatePostfix() {
+    System.`in`.bufferedReader().use { br ->
+        val n = br.readLine().toInt()
+        val postfix = br.readLine()
+        val value: MutableMap<Char, Int> = mutableMapOf()
+        val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for (i in 0 until n) {
+            value[alphabet[i]] = br.readLine().toInt()
+        }
+
+        val operand = Stack<Double>()
+        for (x in postfix) {
+            when (x) {
+                '+' -> {
+                    val tempB = operand.pop()
+                    val tempA = operand.pop()
+                    operand.push(tempA+tempB)
+                }
+                '-' -> {
+                    val tempB = operand.pop()
+                    val tempA = operand.pop()
+                    operand.push(tempA-tempB)
+                }
+                '*' -> {
+                    val tempB = operand.pop()
+                    val tempA = operand.pop()
+                    operand.push(tempA*tempB)
+                }
+                '/' -> {
+                    val tempB = operand.pop()
+                    val tempA = operand.pop()
+                    operand.push(tempA/tempB)
+                }
+                else -> {
+                    operand.push(value[x]!!.toDouble())
+                }
+            }
+        }
+
+        println("${String.format("%.2f", operand.pop())}")
+    }
+}
