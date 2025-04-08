383d4273df9a094c54688cdbc451b03aefba7129
승종 배 <succeeding@nexon.co.kr>
2025-04-08 09:33:46 +0900

    feat: binary search - 나무 자르기

---
+++ kotlin/src/main/kotlin/algorithm/divideandconcquer/binarysearch/나무 자르기.kt
@@ -0,0 +1,34 @@
+package algorithm.divideandconcquer.binarysearch
+
+
+
+fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun i(): Int {
+        nextToken()
+        return nval.toInt()
+    }
+
+    val n = i()
+    val m = i().toLong()
+
+    val heights = Array(n) { i().toLong() }
+    var l = 0L
+    var r = heights.max() + 1
+
+    while (l + 1 < r) {
+        var sum = 0L
+        val h = (l + r) shr 1
+
+        for (height in heights) {
+            if (height > h) sum += height - h
+        }
+
+        if (sum < m) {
+            r = h
+        } else {
+            l = h
+        }
+    }
+
+    println(l)
+}
