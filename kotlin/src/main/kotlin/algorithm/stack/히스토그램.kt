b089bcd56702f6f2d7da499d5db8ae42e47493e1
succeeding <bcj0114@gmail.com>
2025-04-12 23:56:08 +0900

    feat: add stack histogram

---
+++ kotlin/src/main/kotlin/algorithm/stack/히스토그램.kt
@@ -0,0 +1,24 @@
+package algorithm.stack
+
+fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun i(): Double {
+        nextToken()
+        return nval
+    }
+    val n = i().toInt()
+    val histogram = List(n) { i().toLong() } + listOf(0L)
+    val stk = ArrayDeque<Pair<Int, Long>>()
+    var answer = 0L
+
+    for ((endX, currY) in histogram.withIndex()) {
+        var startX = endX
+        while (stk.isNotEmpty() && stk.last().second > currY) {
+            val (prevStartX, prevY)= stk.removeLast()
+            startX = prevStartX // x left sliding until prevY > currY
+            answer = maxOf(answer, (endX - startX) * prevY)
+        }
+        stk.addLast(startX to currY)
+    }
+
+    println(answer)
+}
