872e29142cfdb4b4cb301cf028767c1c3535da57
succeeding <bcj0114@gmail.com>
2025-04-14 21:37:16 +0900

    feat: add dfs 유기농 배추

--- kotlin/src/main/kotlin/algorithm/dfs/유기농 배추.kt
+++ kotlin/src/main/kotlin/algorithm/dfs/유기농 배추.kt
@@ -18,7 +18,30 @@ fun boj1012() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
             val y = i()
             field[x][y] = 1
         }
+        var answer = 0
 
+        fun dfs(x: Int, y: Int) {
+            if (visited[x][y]) return
 
+            visited[x][y] = true
+            if (x + 1 < m && !visited[x + 1][y] && field[x + 1][y] == 1) dfs(x + 1, y)
+            if (y + 1 < n && !visited[x][y + 1] && field[x][y + 1] == 1) dfs(x, y + 1)
+            if (x - 1 > -1 && !visited[x - 1][y] && field[x - 1][y] == 1) dfs(x - 1, y)
+            if (y - 1 > -1 && !visited[x][y - 1] && field[x][y - 1] == 1) dfs(x, y - 1)
+        }
+
+        for (x in 0 until m) {
+            for (y in 0 until n) {
+                if (!visited[x][y]) {
+                    if (field[x][y] == 1) {
+                        dfs(x, y)
+                        answer++
+                    } else {
+                        visited[x][y] = true
+                    }
+                }
+            }
+        }
+        println(answer)
     }
 }
